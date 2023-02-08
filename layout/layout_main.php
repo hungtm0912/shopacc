528f57e7a5b93c28be72e7d87bd73326497b5566
hungtm0912 <hungtm@student.apsce.net>
2023-02-08 15:56:14 +0700

    test

---
+++ layout/layout_main.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+	<link rel="stylesheet" href="../CSS/style1.css">
+	<title>Website Bai Tap</title>
+</head>
+<body>
+	<div class = "layout">
+		<div class = "header">
+			<div id="menu">
+				
+				<ul>
+				<img class="logo_img" src="../img/logo_header.png" alt="">
+				<li><a href="#">Trang chủ</a></li>
+				<li><a href="#">Diễn đàn</a></li>
+				<li><a href="#">Tin tức</a>
+				<ul class="sub-menu">
+				<li><a href="#">WordPress</a></li>
+				<li><a href="#">SEO</a></li>
+				<li><a href="#">Hosting</a></li>
+				</ul>
+				</li>
+				<li><a href="#">Hỏi đáp</a></li>
+				<li><a href="#">Liên hệ</a></li>
+				<li><a href="#">Đăng nhập</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class ="container">
+		</div>
+		<div class="layout">
+		</div>
+	</div>
+</body>
+</html>
